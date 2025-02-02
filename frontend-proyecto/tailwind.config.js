919e7b7f5c2a6a3dfa781599f8bd603a73ab61ee
bugNetwork <raynerro2107@gmail.com>
2025-02-02 11:39:17 -0400

    Login creado

---
+++ frontend-proyecto/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}', // Asegúrate de incluir todos tus archivos de código fuente
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
